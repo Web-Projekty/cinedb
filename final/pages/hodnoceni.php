9888ee79a092ee37cf35a99bafe718a23f776049
Wlczak <adivlcak@seznam.cz>
2023-05-15 20:55:12 +0200

    Podmínka pro přihlášení při hodnocení stránky

--- final/pages/hodnoceni.php
+++ final/pages/hodnoceni.php
@@ -47,94 +47,111 @@ include "../account/timed_log_out.php";
         <section class="rating">
             <form method="post">
 
-                <?php if (isset($_POST['review']) && empty($_POST['review'])) {
-                    echo "<input type='text' name='review' placeholder='Toto pole je povinné' style='border: 1px solid red;'>";
-                } elseif (isset($_POST['review']) && $_POST['review'] != NULL) {
-                    $review = $_POST['review'];
-                    echo "<input type='text' name='review' placeholder='Recenze stránky...'value='$review' style='border: 1px solid black;'>";
-                } else {
-                    echo "<input type='text' name='review' placeholder='Recenze stránky...' style='border: 1px solid black;'>";
-                } ?>
-                <input type="radio" name="star" class="hidden" value="0" checked>
-                <label><input type="radio" name="star" class="hidden" value="1">
-                    <a class="star_on" onclick="star(1)" id="star_1">★</a> </label>
-                <label><input type="radio" name="star" class="hidden" value="2">
-                    <a class="star_off" onclick="star(2)" id="star_2">★</a> </label>
-                <label><input type="radio" name="star" class="hidden" value="3">
-                    <a class="star_off" onclick="star(3)" id="star_3">★</a> </label>
-                <label><input type="radio" name="star" class="hidden" value="4">
-                    <a class="star_off" onclick="star(4)" id="star_4">★</a> </label>
-                <label><input type="radio" name="star" class="hidden" value="5">
-                    <a class="star_off" onclick="star(5)" id="star_5">★</a> </label>
-                <?php if (isset($_POST['star']) && empty($_POST['star'])) {
-                    echo "<a style='color:red;'>Toto pole je povinné</a>";
-                }
-                ?>
-                <button type="submit" name="submit" value="1">odeslat recenzi</button>
-            </form>
-            <?php
-            $filename = "../txt/hodnoceni.txt";
-            if (!empty($_POST['review']) && !empty($_POST['star']) && isset($_POST['review']) && isset($_POST['star']) && $_POST['star'] != 0) {
-                if (!empty($_POST['submit']) && $_POST['submit'] = 1) {
-                    if (file_exists("../txt/hodnoceni.txt")) {
-                        $filename = "../txt/hodnoceni.txt";
-                        $file = fopen($filename, "a");
-
-                        if (filesize($filename) == 0) { //je potřeba odřádkovat??
-                            $write = $_POST['review'] . "|" . $_POST['star'];
-                        } else {
-                            $write = "\n" . $_POST['review'] . "|" . $_POST['star'];
-                        }
-                        fwrite($file, $write);
-                        echo "vaše hodnocení bylo úspěšně zaznamenáno";
+                <?php
+                $filename = "../txt/hodnoceni.txt";
+                if ($_SESSION['user'] == true) {
+                    if (isset($_POST['review']) && empty($_POST['review'])) {
+                        echo "<input type='text' name='review' placeholder='Toto pole je povinné' style='border: 1px solid red;'>";
+                    } elseif (isset($_POST['review']) && $_POST['review'] != NULL) {
+                        $review = $_POST['review'];
+                        echo "<input type='text' name='review' placeholder='Recenze stránky...'value='$review' style='border: 1px solid black;'>";
                     } else {
-                        echo "<p><a style='font-weight: bold;'>File error: </a>204</p>";
+                        echo "<input type='text' name='review' placeholder='Recenze stránky...' style='border: 1px solid black;'>";
                     }
-                }
-                fclose($file);
-            }
 
-            //výpis hodnocení
-            if (filesize($filename) != 0) {
-                $sum = 0;
-                $counter = 0;
-                $file = fopen($filename, "r");
-                echo "<table>
+                    if (isset($star)) {
+                        $star = $_POST['star'];
+                    }
+                    echo "
+                <input type='radio' name='star' class='hidden' value='0' checked>
+                <label><input type='radio' name='star' class='hidden' value='1'>
+                    <a class='star_on' onclick='star(1)' id='star_1'>★</a> </label>
+                <label><input type='radio' name='star' class='hidden' value='2'>
+                    <a class='star_off' onclick='star(2)' id='star_2'>★</a> </label>
+                <label><input type='radio' name='star' class='hidden' value='3'>
+                    <a class='star_off' onclick='star(3)' id='star_3'>★</a> </label>
+                <label><input type='radio' name='star' class='hidden' value='4'>
+                    <a class='star_off' onclick='star(4)' id='star_4'>★</a> </label>
+                <label><input type='radio' name='star' class='hidden' value='5'>
+                    <a class='star_off' onclick='star(5)' id='star_5'>★</a> </label>";
+                    if (isset($star) && empty($star)) {
+                        echo "<a style='color:red;'>Toto pole je povinné</a>";
+                    }
+                    echo "
+                <button type='submit' name='submit' value='1'>odeslat recenzi</button>
+            </form>";
+
+                    //zápis do souboru
+
+                    if (!empty($_POST['review']) && !empty($_POST['star']) && isset($_POST['review']) && isset($_POST['star']) && $_POST['star'] != 0) {
+                        if (!empty($_POST['submit']) && $_POST['submit'] = 1) {
+                            if (file_exists("../txt/hodnoceni.txt")) {
+                                $filename = "../txt/hodnoceni.txt";
+                                $file = fopen($filename, "a");
+
+                                if (filesize($filename) == 0) { //je potřeba odřádkovat??
+                                    $write = $_POST['review'] . "|" . $_POST['star'] . "|" . $_SESSION['username'];
+                                } else {
+                                    $write = "\n" . $_POST['review'] . "|" . $_POST['star'];
+                                }
+                                fwrite($file, $write);
+                                echo "vaše hodnocení bylo úspěšně zaznamenáno";
+                            } else {
+                                echo "<p><a style='font-weight: bold;'>File error: </a>204</p>";
+                            }
+                        }
+                        fclose($file);
+                    }
+                } else {
+                    echo "prosím přihlaš se než ohodnotíš stánku";
+                }
+                //výpis hodnocení
+                if (filesize($filename) != 0) {
+                    $sum = 0;
+                    $counter = 0;
+                    $file = fopen($filename, "r");
+                    echo "<table>
     <tr>
         <th>Recenze</th>
         <th>
             Hodnocení
         </th>
+        <th>
+            Datum
+        </th>
+        <th>
+            Uživatel
+        </th>
     </tr>";
-                while (!feof($file)) {
-                    $line = fgets($file);
-                    $values = explode("|", $line);
-                    echo "
+                    while (!feof($file)) {
+                        $line = fgets($file);
+                        $values = explode("|", $line);
+                        echo "
         <tr>
             <td>$values[0]</td>
             <td>";
-                    for ($i = 0; $i < 5; $i++) {
-                        if ($i < $values[1]) {
-                            echo "<a class='star_on'>★</a>";
-                        } else {
-                            echo "<a class='star_off'>★</a>";
+                        for ($i = 0; $i < 5; $i++) {
+                            if ($i < $values[1]) {
+                                echo "<a class='star_on'>★</a>";
+                            } else {
+                                echo "<a class='star_off'>★</a>";
+                            }
                         }
-                    }
-                    //průměr hodnocení
-                    $sum += $values[1];
-                    $counter += 1;
-                    echo "</td>
+                        //průměr hodnocení
+                        $sum += $values[1];
+                        $counter += 1;
+                        echo "</td>
               </tr>";
-                }
-                echo "<tr>
+                    }
+                    echo "<tr>
     <th>Průměrné hodnocení</th>
       <td>";
-                echo round($sum / $counter, 2);
-                echo "</td>
+                    echo round($sum / $counter, 2);
+                    echo "</td>
 </tr>
 </table>";
-            }
-            ?>
+                }
+                ?>
         </section>
 
         <?php include "../include/footer.php" ?>
