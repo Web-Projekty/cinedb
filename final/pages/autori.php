4c89cd15db958f2b907849e5e44e9f20af85b9bb
florixak <florixakcz@seznam.cz>
2023-05-15 09:43:44 +0200

    Změna titlu

--- final/pages/autori.php
+++ final/pages/autori.php
@@ -10,7 +10,7 @@ include "../account/timed_log_out.php"; ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Databáze seriálů - Autoři</title>
+    <title>CineDB - Autoři</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/autori.css">
 
