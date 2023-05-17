8375ffee969e76b8f1a63a7af272864afac9cd3a
florixak <florixakcz@seznam.cz>
2023-05-17 09:01:34 +0200

    Úpravy css

--- final/pages/detaily.php
+++ final/pages/detaily.php
@@ -3,98 +3,113 @@ $_SESSION['page'] = 2;
 include "../account/timed_log_out.php"; ?>
 <!DOCTYPE html>
 <html lang="cs">
-<style>
-    table {
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-
-    table td,
-    tr,
-    th {
-        border: 1px solid black;
-    }
-
-    img {
-        width: 100px;
-    }
-</style>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detaily</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/detaily.css">
 </head>
 
-<?php
-//Nelze otevřít v případě, že je otevřený přímo
-include "../db/active_db.php";
+<body>
+    <div class="flex-box">
+        <header>
+            <div class="header-title">
+                <img src="../img/logo/logo_exp_wStroke.png" alt="logo">
+            </div>
 
-$idS = $_GET['idS'];
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+            <nav>
+                <ul class="header-nav">
+                    <li><a href="../index.php">Úvod</a></li>
+                    <li><a href="serialy.php">Seriály</a></li>
+                    <li><a href="autori.php">Autoři</a></li>
+                    <li><a href="statistiky.php">Statistiky</a></li>
+                    <li><a href="hodnoceni.php">Hodnocení</a></li>
+                </ul>
+            </nav>
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+            <div class="header-login">
+                <ul>
+                    <li>
+                        <?php include "../account/user_status.php"; ?>
+                    </li>
+                </ul>
+            </div>
+        </header>
 
-$sql = "SELECT serialy.idS, serialy.nazev, serialy.idA, autori.jmeno, autori.prijmeni, type from serialy inner join autori on serialy.idA = autori.idA WHERE idS = $idS";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc(); ?>
-    <h2>Detaily k seriálu <?php echo $row['nazev']; ?></h2>
-    <table>
-        <tr>
-            <th>Obrázek</th>
-            <th>ID</th>
-            <th>Název</th>
-            <th>ID Autora</th>
-            <th>Jméno autora</th>
-            <th>Příjmení autora</th>
-            <th>hodnocení</th>
-            <th>typ</th>
-            <th>zpět</th>
-        </tr>
+        <section class="details">
+            <?php
+                //Nelze otevřít v případě, že je otevřený přímo
+                include "../db/active_db.php";
 
-<body>    
-    <form action="" method="POST">
-        <textarea name="recenze" id="recenze" cols="80" rows="5"></textarea>
-        <input type="submit" value="odeslat recenzi" name="odeslat">
+                $idS = $_GET['idS'];
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
 
-    </form>
-        <?php
-        $sql = "SELECT hodnota from hodnoceni WHERE idS = '$idS'";
-        $rating_result = $conn->query($sql);
-        $counter = 0;
-        $total = 0;
-        while ($rating = $rating_result->fetch_assoc()) {
-            $counter++;
-            $total += $rating['hodnota'];
-        }
-        if ($counter < 1) {
-            $ratingAVG = "žádné recenze";
-        } else {
-            $ratingAVG = $total / $counter;
-        }
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
 
-        if ($result->num_rows > 0) {
-            echo "<tr>
-            <td><img src='../img/db/$idS.jpg'></td>" .
-                "<td>" . $row['idS'] . "</td>
-            <td>" . $row['nazev'] . "</td>
-            <td>" . $row['idA'] . "</td>
-            <td>" . $row['jmeno'] . "</td>
-            <td>" . $row['prijmeni'] . "</td>
-            <td>" . $ratingAVG . "</td>
-            <td>" . $row['type'] . "</td>
-            <td><a href='serialy.php'>zpět</a>
-            </tr>";
-        }
-        $conn->close();
-        }
-        ?>
+                $sql = "SELECT serialy.idS, serialy.nazev, serialy.idA, autori.jmeno, autori.prijmeni, type from serialy inner join autori on serialy.idA = autori.idA WHERE idS = $idS";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();?>
+                    <h2>Detaily k seriálu <?php echo $row['nazev']; ?></h2>
+                    <table>
+                        <tr>
+                            <th>Obrázek</th>
+                            <th>ID</th>
+                            <th>Název</th>
+                            <th>ID Autora</th>
+                            <th>Jméno autora</th>
+                            <th>Příjmení autora</th>
+                            <th>Hodnocení</th>
+                            <th>Typ</th>
+                            <th>Zpět</th>
+                        </tr>
+                <?php
+                    $sql = "SELECT hodnota from hodnoceni WHERE idS = '$idS'";
+                    $rating_result = $conn->query($sql);
+                    $counter = 0;
+                    $total = 0;
+                    while ($rating = $rating_result->fetch_assoc()) {
+                        $counter++;
+                        $total += $rating['hodnota'];
+                    }
+                    if ($counter < 1) {
+                        $ratingAVG = "žádné recenze";
+                    } else {
+                        $ratingAVG = $total / $counter;
+                    }
+
+                    if ($result->num_rows > 0) {
+                        echo "<tr>
+                        <td><img src='../img/db/$idS.jpg'></td>" .
+                            "<td>" . $row['idS'] . "</td>
+                        <td>" . $row['nazev'] . "</td>
+                        <td>" . $row['idA'] . "</td>
+                        <td>" . $row['jmeno'] . "</td>
+                        <td>" . $row['prijmeni'] . "</td>
+                        <td>" . $ratingAVG . "</td>
+                        <td>" . $row['type'] . "</td>
+                        <td><a href='serialy.php'>zpět</a>
+                        </tr>";
+                    }
+                    $conn->close();
+                    }
+                ?>
+
+            <form action="" method="POST">
+                <textarea name="recenze" id="recenze" cols="50" rows="5"></textarea>
+                <input type="submit" value="odeslat recenzi" name="odeslat">
+            </form>
+        </section>
+        
+        <?php include "../include/footer.php" ?>
+    </div>
 </body>
 
 </html>
