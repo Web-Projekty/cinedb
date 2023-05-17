a9dc886ea5b9217ade99bc042f86814e4b46898e
Rehakj <132693892+Rehakj@users.noreply.github.com>
2023-05-17 13:57:07 +0200

    update detaily (pořád to nefunguje)

--- final/db/active_db.php
+++ final/db/active_db.php
@@ -1,5 +1,5 @@
 <?php //zde bude aktivní připojení pro SQL databázi
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "localhost:3306";
+$username = "cineDB";
+$password = "#BDenic2305";
 $dbname = "cineDB";
