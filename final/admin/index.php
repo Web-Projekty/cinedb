12297268277562d13f6c0173ac48979a6f1209e2
Wlczak <adivlcak@seznam.cz>
2023-05-19 16:31:29 +0200

    TV show edit + create + delete

--- final/admin/index.php
+++ final/admin/index.php
@@ -9,15 +9,56 @@ include "verification.php"; ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Pták, Vlček, Rehák">
+    <meta name="keywords" content="databáze seriálů, hodnocení seriálů">
+    <meta name="description" conntent="Databáze dostupných seriálů a filmů. Podle hodnocení se můžete zvážit zda seriál či film stojí za váš čas a pozornost.">
+
     <title>Admin menu</title>
+
+    <link rel="shortcut icon" href="../img/logo/logo_icon_exp.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/uvod.css">
 </head>
 
 <body>
-    <h2>Admin super menu</h2>
-    <section>
-        <a href="show_add.php"> <button>přidat seriály</button></a>
-        <a href="author_add.php"> <button>přidat autory</button></a>
-    </section>
+    <div class="flex-box">
+        <header>
+            <div class="header-title">
+                <img src="../img/logo/logo_exp_wStroke.png" alt="logo">
+            </div>
+
+            <nav>
+                <ul class="header-nav">
+                    <li><a href="../index.php" id="selected">Úvod</a></li>
+                    <li><a href="../pages/serialy.php">Seriály</a></li>
+                    <li><a href="../pages/autori.php">Autoři</a></li>
+                    <li><a href="../pages/statistiky.php">Statistiky</a></li>
+                    <li><a href="../pages/hodnoceni.php">Hodnocení</a></li>
+                </ul>
+            </nav>
+
+            <div class="header-login">
+                <ul>
+                    <li>
+                        <?php include "../account/user_status.php"; ?>
+                    </li>
+                </ul>
+            </div>
+        </header>
+
+        <section class="about-website">
+            <h2>Admin super menu</h2>
+            <section>
+                <a href="show_add.php"> <button>Seriály</button></a>
+                <a href="author_add.php"> <button>Autory</button></a>
+            </section>
+
+        </section>
+
+        <?php include "../include/footer.php" ?>
+    </div>
+
 </body>
 
 </html>
