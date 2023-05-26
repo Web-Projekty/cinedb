359bebd80075f5be9acca748f80a998ba290d6ab
Wlczak <adivlcak@seznam.cz>
2023-05-26 20:43:34 +0200

    Profile picture upload

--- final/account/user_profile.php
+++ final/account/user_profile.php
@@ -43,8 +43,8 @@
         </header>
 
         <section class="user-profile">
-            <?php
 
+            <?php
             if ($_SESSION['user'] == true) {
                 $profileId = $_SESSION['profileId'];
                 $user = $_SESSION['username'];
@@ -71,8 +71,66 @@
                         echo "<a href='../admin'><button>admin panel</button></a>";
                     }
                 }
+                echo "<br><form class='picture-upload' method='post' enctype='multipart/form-data'>
+                <input type='file' name='file_upload'>
+                <button type='submit' name='submit' value='true'>upload</button>
+            </form>";
+                if (!empty($_POST['submit'])) {
+                    include "../db/active_db.php";
+
+                    // Create connection
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "SELECT MAX(profileId) as 'maxprofile' FROM users";
+
+                    $result = $conn->query($sql);
+                    //echo $conn->info;
+                    $row = $result->fetch_assoc();
+                    $profileId = $row['maxprofile'] + 1;
+                    $user = $_SESSION['username'];
+                    $sql = "UPDATE users SET profileId = '$profileId' WHERE username = '$user'";
+                    $result = $conn->query($sql);
+                    $_SESSION['profileId'] = $profileId;
+
+                    $uploadOk = 1;
+                    $filename = $profileId . ".png";
+                    $target_dir = "../img/profile/";
+                    $target_file = $target_dir . $filename; //basename($_FILES["file_upload"]["name"]);
+                    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                    //var_dump($imageFileType);
+                    if (isset($_POST["submit"])) {
+                        $check = getimagesize($_FILES["file_upload"]["tmp_name"]);
+                        if ($check !== false) {
+                            //echo "<br>File is an image - " . $check["mime"] . ".<br>";
+                            $uploadOk = 1;
+                        } else {
+                            echo "<br>Typ nahraného souboru nepodporujeme";
+                            $uploadOk = 0;
+                        }
+                    }
+                    if ($imageFileType != "png") {
+                        echo "<br>Typ nahraného souboru nepodporujeme";
+                        $uploadOk = 0;
+                    }
+                    if ($uploadOk == 0) {
+                        echo "<br>Omlouváme se, ale váše profilová fotka nebyla změněna";
+                        // if everything is ok, try to upload file
+                    } else {
+                        if (move_uploaded_file($_FILES["file_upload"]["tmp_name"], $target_file)) {
+                            echo "<br>Profilový obrázek úspěšně změněn";
+                        } else {
+                            echo "<br>Sorry, there was an error uploading your file.";
+                        }
+                    }
+                } else {
+                    echo "<br>Prosím zvolte svůj profilový obrázek";
+                }
             } else {
-                echo "prosím přihlašte se";
+                echo "Prosím přihlašte se";
             }
             ?>
 
