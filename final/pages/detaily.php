dd6f17cbd4a572ec75fadafa079eae20a3a27203
Wlczak <adivlcak@seznam.cz>
2023-05-30 14:50:49 +0200

    Google analytics

--- final/pages/detaily.php
+++ final/pages/detaily.php
@@ -7,6 +7,18 @@ include "../account/timed_log_out.php";
 <html lang="cs">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5N5BJHQ74R"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-5N5BJHQ74R');
+    </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
