18368eff0200b5d7b6939e0ab829c517aef1e580
florixak <florixakcz@seznam.cz>
2023-05-30 13:42:50 +0200

    Update

--- final/pages/hodnoceni.php
+++ final/pages/hodnoceni.php
@@ -46,7 +46,7 @@ include "../account/timed_log_out.php";
         </header>
 
         <section class="rating">
-            <form method="post">
+            <form action="" method="post">
 
                 <?php
                 $filename = "../txt/hodnoceni.txt";
