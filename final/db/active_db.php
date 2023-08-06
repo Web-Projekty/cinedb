4a81acbc17c5e52c674fda66898fdb61eed86471
Wlczak <adivlcak@seznam.cz>
2023-08-06 12:51:29 +0200

    Úprava navbaru + registrace účtů

--- final/db/active_db.php
+++ final/db/active_db.php
@@ -1,5 +1,5 @@
 <?php //zde bude aktivní připojení pro SQL databázi
-$servername = "localhost:3306";
-$username = "cineDB";
-$password = "#BDenic2305";
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "cineDB";
